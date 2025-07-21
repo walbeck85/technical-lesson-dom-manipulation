5d6668509f63ede1138c7952059502839cf54879
Will Albeck <will@albeck.me>
2025-07-21 16:04:58 -0400

    Complete DOM Manipulation Lab: Add user display logic and debugging logs

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // Write your code here!
+const userInfo = {
+	username: "flatUser123",
+	description: "Hi, welcome to my webpage! Just a new user of this website!",
+	theme: "red"
+}
 
+function displayUser(userInfo) {
+  console.log("Running displayUser with:", userInfo);
+
+  const header = document.querySelector("#main");
+  console.log("Selected header element:", header);
+
+  header.textContent = `${userInfo.username}'s Personal Webpage`;
+  header.className = userInfo.theme;
+  console.log("Updated header text and theme class");
+
+  const description = document.createElement("p");
+  console.log("Created new <p> element:", description);
+
+  description.textContent = userInfo.description;
+  description.className = userInfo.theme;
+  console.log("Set description text and theme class");
+
+  const body = document.querySelector("body");
+  console.log("Selected body element:", body);
+
+  body.append(description);
+  console.log("Appended description to the body");
+}
+
+displayUser(userInfo)
